97663e9e323bae6d3e442f4d67ec09205bb3cea2
eliasvitali <elias.vitali@gmail.com>
2024-07-24 15:47:57 +0200

    default values, adjusted formulas, and cost per hour

--- calculator.js
+++ calculator.js
@@ -1,23 +1,23 @@
-function calculateCosts(costPerHour, totalTime, timeAPIC, timeASafety, timeBPIC, timeBSafety) {
-    if (timeAPIC + timeBPIC > totalTime) {
+function calculateCosts(totalTime, timeA_PIC, timeB_safety, timeB_PIC, timeA_safety, rentalHourlyRate) {
+    if (timeA_PIC + timeB_PIC > totalTime) {
         throw new Error("The sum of PIC times cannot exceed the total time.");
     }
-    if (timeASafety > timeBPIC) {
+    if (timeA_safety > timeB_PIC) {
         throw new Error("Person A's safety time cannot exceed person B's PIC time.");
     }
-    if (timeBSafety > timeAPIC) {
+    if (timeB_safety > timeA_PIC) {
         throw new Error("Person B's safety time cannot exceed person A's PIC time.");
     }
 
-    const costA = (timeAPIC - timeBSafety) * costPerHour + (costPerHour * timeBSafety / 2);
-    const totalCost = costPerHour * totalTime;
-    const costB = totalCost - costA;
+    const costA = ((timeA_PIC - timeB_safety) * rentalHourlyRate) + (timeB_safety * rentalHourlyRate / 2) + (timeA_safety * rentalHourlyRate / 2);
+    const costB = (timeB_safety * rentalHourlyRate / 2) + ((timeB_PIC - timeA_safety) * rentalHourlyRate) + (timeA_safety * rentalHourlyRate / 2);
 
-    const loggedTimeA = timeAPIC + timeASafety;
-    const loggedTimeB = timeBPIC + timeBSafety;
+    const loggedTimeA = timeA_PIC + timeA_safety;
+    const loggedTimeB = timeB_PIC + timeB_safety;
 
     const costPerHourA = loggedTimeA !== 0 ? costA / loggedTimeA : 0;
     const costPerHourB = loggedTimeB !== 0 ? costB / loggedTimeB : 0;
+    const flightCost = rentalHourlyRate * totalTime;
 
     return {
         costA,
@@ -25,6 +25,7 @@ function calculateCosts(costPerHour, totalTime, timeAPIC, timeASafety, timeBPIC,
         loggedTimeA,
         costPerHourA,
         loggedTimeB,
-        costPerHourB
+        costPerHourB,
+        flightCost
     };
 }
